284b15af5daaab4f5aac5b0d0c4a92c739e8e9e9
taebassi <athanon18@hotmail.com>
2016-09-07 00:46:33 +0700

    tae(6/9/59 00:46)

--- app/src/main/java/com/example/taeba/pethome/MainActivity.java
+++ app/src/main/java/com/example/taeba/pethome/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.taeba.pethome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LogInActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
